14b2dfd45e32c2a27d6b4d739ce24d32372f8689
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-11 03:34:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -80,7 +80,9 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    
+    while (cart.length) {
+      cart.pop();
+  }
   } else {
     return("Sorry, we don't have a credit card on file for you.")
   }
