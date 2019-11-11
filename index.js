fc815a0141e7943b6d79d1059a599ef9b3b27744
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-11 03:10:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     var empty_cart = cart
     cart = []
-    return empty_cart
+    empty_cart
     
   } else {
     return("Sorry, we don't have a credit card on file for you.")
