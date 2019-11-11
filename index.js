9e757c663fba9c66e770b133803f90d6e5315d19
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-11 03:20:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,8 +79,8 @@ function removeFromCart(remove_item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart.splice(0, cart.length)
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   } else {
     return("Sorry, we don't have a credit card on file for you.")
   }
